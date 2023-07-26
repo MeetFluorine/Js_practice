11bcd26572a322304968c7060b32b277dfbeee33
Chandan Yadav <103582224+MeetFluorine@users.noreply.github.com>
2023-07-26 23:23:04 +0000

    bmi converter

---
+++ BMIcalculator/script.js
@@ -0,0 +1,21 @@
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const h =  parseInt(document.querySelector('#height').value);
+    const w = parseInt(document.querySelector('#weight').value);
+    let display = document.querySelector('.result');
+
+    if(h === '' || h<0 || isNaN(h) ){
+        display.innerHTML= "please enter a valid height"
+    }
+    if(w === '' || w<0 || isNaN(w) ){
+        display.innerHTML= "please enter a valid weight"
+    }
+     
+    
+
+    else{
+        const bmi = w/((h**2)/10000).toFixed(2)
+        display.innerHTML = bmi;
+    }
+
+})
